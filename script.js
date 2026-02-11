8df76408ad8bfd6bac23cc3e6457760220d31c4c
Kamran-Supreme06 <133527987+Kamran-Supreme06@users.noreply.github.com>
2026-02-11 08:43:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+const photos = document.querySelectorAll(".photos img");
+let currentPhoto = 0;
+const memories = document.getElementById("memories");
+
+// Show memories AFTER intro text
+setTimeout(() => {
+  memories.style.opacity = 1;
+}, 6500);
+
+
+setInterval(() => {
+  photos[currentPhoto].classList.remove("active");
+  currentPhoto = (currentPhoto + 1) % photos.length;
+  photos[currentPhoto].classList.add("active");
+}, 3000);
+
+const question = document.getElementById("question");
+
+// Show question AFTER text finishes
+setTimeout(() => {
+  question.style.opacity = 1;
+}, 8000);
+
+
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+
+let noClickCount = 0;
+
+const noTexts = [
+  "Are you sure? 🥺",
+  "That would make me sad 💔",
+  "You don’t want to click me 😭",
+  "Think again 😳",
+  "Okay but… why 😩"
+];
+
+noBtn.addEventListener("click", () => {
+  // Change text
+  noBtn.textContent = noTexts[noClickCount % noTexts.length];
+
+  // Move NO button
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 200 - 100;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+
+  // Grow YES button
+  noClickCount++;
+  let scale = 1 + noClickCount * 0.25;
+  yesBtn.style.transform = `scale(${scale})`;
+});
+
+yesBtn.addEventListener("click", () => {
+  document.body.innerHTML = `
+    <div style="
+      height:100vh;
+      display:flex;
+      flex-direction:column;
+      justify-content:center;
+      align-items:center;
+      background:linear-gradient(135deg,#ff5f9e,#ffd6e8);
+      color:white;
+      text-align:center;
+      padding:20px;
+      gap:15px;">
+
+      <img src="teddy.gif" alt="Teddy Bear Love" style="width:180px;">
+
+      <h1>YAYYY 💖🎉</h1>
+      <h2>You just made me the happiest man ever 😌</h2>
+      <h2>I can't wait to spend Valentine’s Day with you my love 💕</h2>
+
+    </div>
+  `;
+});
+
+
+
+
